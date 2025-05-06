dbf5e99a635754eb008609abe0ec4aca50d97a3f
Toby Dylan Hocking <toby.hocking@r-project.org>
2025-05-06 20:57:25 +0200

    tikz figures

--- figure-heap.R
+++ figure-heap.R
@@ -33,6 +33,29 @@ png("figure-heap-refs-other.png",width=6,height=3.5,units="in",res=200)
 plot_algos("change|rupt")
 dev.off()
 
+tikz_algos <- function(regex){
+  regex_list <- ref_list
+  regex_list$measurements <- ref_list$measurements[grepl(regex,expr.name)]
+  regex_list$plot.references <- ref_list$plot.references[grepl(regex,expr.name)][
+   ,fun.name := sprintf("$%s$", sub("1.5","{1.5}",fun.latex))][]
+  plot(regex_list)+
+    geom_blank(aes(
+      N, empirical),
+      data=ref_list$measurements[,.(N=1000,empirical,unit)])+
+    scale_x_log10(
+      breaks=10^seq(1,7,by=1))+
+    theme(
+      axis.text.x=element_text(angle=30,hjust=1))
+}
+library(tikzDevice)
+tikz("figure-heap-refs-binsegRcpp.tex",width=6,height=3)
+tikz_algos("list|multiset")
+dev.off()
+
+tikz("figure-heap-refs-other.tex")
+tikz_algos("change|rupt")
+dev.off()
+
 pred_list <- predict(ref_list)
 png("figure-heap-pred.png",width=8,height=3,units="in",res=200)
 pkg.colors <- c(
