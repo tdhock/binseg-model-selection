c99387b9ea89bbd0141c4da33cbdbbd5e4dcd78f
Toby Dylan Hocking <toby.hocking@r-project.org>
2025-05-07 08:45:18 +0200

    N^1.4

--- figure-heap.R
+++ figure-heap.R
@@ -4,7 +4,8 @@ atime_list$measurements[, expr.name := sub("container=", "", expr.name)]
 plot(atime_list)
 
 fun_list <- atime:::references_funs[c("N","N \\log N", "N^2", "N^3")]
-fun_list[["N^1.5"]] <- function(N)1.5 * log10(N)
+myexp <- 1.4
+fun_list[[paste0("N^",myexp)]] <- function(N)myexp * log10(N)
 ref_list <- atime::references_best(atime_list, fun_list)
 ref_list$plot.references <- ref_list$references[(
   fun.name %in% c("N","N^2") & unit=="kilobytes"
@@ -37,10 +38,41 @@ png("figure-heap-refs-other.png",width=6,height=3.5,units="in",res=200)
 plot_algos("change|rupt")
 dev.off()
 
+pred_list <- predict(ref_list)
+png("figure-heap-pred.png",width=8,height=3,units="in",res=200)
+pkg.colors <- c(
+  changepoint="#E41A1C",
+  ruptures="#377EB8",
+  "binsegRcpp\nmultiset"="black",
+  "binsegRcpp\npriority_queue"="grey30",
+  "binsegRcpp\nlist"="grey50")
+plot(pred_list)+
+  facet_null()+
+  scale_y_log10(
+    "Computation time (seconds, log scale)",
+    breaks=10^seq(-3,1),
+    limits=c(NA,200))+
+  scale_x_log10(
+    "N = number of data to segment (max segments = N/2)",
+    breaks=10^seq(1,7),
+    limits=c(NA, 1e8))+
+  scale_color_manual(values=pkg.colors)+
+  scale_fill_manual(values=pkg.colors)
+dev.off()
+
+norm_dt <- pred_list$prediction[, ratio := max(N)/N][, .(expr.name, ratio)][pred_list$meas, on="expr.name"][unit=="seconds"][, norm_N := N*ratio][]
+library(ggplot2)
+ggplot()+
+  geom_line(aes(
+    norm_N, empirical, color=expr.name),
+    data=norm_dt)+
+  scale_x_log10()+
+  scale_y_log10()
+
 tikz_algos <- function(f.tex,regex){
   trans <- function(DT)DT[,expr.name:=sub("_","\\_",expr.name,fixed=TRUE)]
   meas <- trans(ref_list$measurements[grepl(regex,expr.name)])
-  ref.dt <- trans(ref_list$plot.references[grepl(regex,expr.name)])[,fun.name := sprintf("$%s$", sub("1.5","{1.5}",fun.latex))][]
+  ref.dt <- trans(ref_list$plot.references[grepl(regex,expr.name)])[,fun.name := sprintf("$%s$", sub(paste(myexp),paste0("{",myexp,"}"),fun.latex))][]
   ##plot(regex_list)+
   emp.color <- "black"
   ref.color <- "violet"
@@ -86,33 +118,3 @@ library(tikzDevice)
 tikz_algos("figure-heap-refs-binsegRcpp.tex", "list|multiset")
 tikz_algos("figure-heap-refs-other.tex", "change|rupt")
 
-pred_list <- predict(ref_list)
-png("figure-heap-pred.png",width=8,height=3,units="in",res=200)
-pkg.colors <- c(
-  changepoint="#E41A1C",
-  ruptures="#377EB8",
-  "binsegRcpp\nmultiset"="black",
-  "binsegRcpp\npriority_queue"="grey30",
-  "binsegRcpp\nlist"="grey50")
-plot(pred_list)+
-  facet_null()+
-  scale_y_log10(
-    "Computation time (seconds, log scale)",
-    breaks=10^seq(-3,1),
-    limits=c(NA,1e2))+
-  scale_x_log10(
-    "N = number of data to segment (max segments = N/2)",
-    breaks=10^seq(1,7),
-    limits=c(NA, 1e8))+
-  scale_color_manual(values=pkg.colors)+
-  scale_fill_manual(values=pkg.colors)
-dev.off()
-
-norm_dt <- pred_list$prediction[, ratio := max(N)/N][, .(expr.name, ratio)][pred_list$meas, on="expr.name"][unit=="seconds"][, norm_N := N*ratio][]
-library(ggplot2)
-ggplot()+
-  geom_line(aes(
-    norm_N, empirical, color=expr.name),
-    data=norm_dt)+
-  scale_x_log10()+
-  scale_y_log10()
